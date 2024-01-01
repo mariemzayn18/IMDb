23bf2c2ae8d2e02f4dca5d0c50245e29aa6e0b2f
mariemzayn18 <mariamzayn18@gmail.com>
2024-01-01 13:16:24 +0200

    Exchange between sign in & signup

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,6 +8,10 @@ import { NgForm } from '@angular/forms';
 })
 export class RegistrationComponent {
   title = 'Sign in';
+  newOrHavingAccount = 'New to IMDb?';
+  exchangeButtonText = 'Create your IMDb account';
+  submitButtonText = 'Sign in';
+
   formFields = [
     {
       label: 'Email',
@@ -29,5 +33,20 @@ export class RegistrationComponent {
 
   onSubmit(form: NgForm) {
     console.log(form);
+  }
+
+  exchangeButtons() {
+    this.title = this.title === 'Sign in' ? 'Create account' : 'Sign in';
+    this.newOrHavingAccount =
+      this.newOrHavingAccount === 'New to IMDb?'
+        ? 'Already have an account?'
+        : 'New to IMDb?';
+    this.exchangeButtonText =
+      this.exchangeButtonText === 'Create your IMDb account'
+        ? 'Sign in'
+        : 'Create your IMDb account';
+
+    this.submitButtonText =
+      this.submitButtonText === 'Sign in' ? 'Sign Up' : 'Sign in';
   }
 }
