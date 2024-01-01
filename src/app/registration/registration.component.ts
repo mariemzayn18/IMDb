0c20e643185762374aaa6e8ca378e146b55dc38f
mariemzayn18 <mariamzayn18@gmail.com>
2024-01-01 11:34:46 +0200

    Added validations

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class RegistrationComponent {
   title = 'Sign in';
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
