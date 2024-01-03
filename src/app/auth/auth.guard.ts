8f13139c43259c4dd05f1f30ddeb5a0e5e7d9250
mariemzayn18 <mariamzayn18@gmail.com>
2024-01-03 13:03:09 +0200

    edited rerouting path when not authenticated

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -29,7 +29,7 @@ export class AuthGuard implements CanActivate {
         if (isAuthenticated) {
           return true;
         }
-        return this.router.createUrlTree(['/auth']);
+        return this.router.createUrlTree(['/']);
       })
     );
   }
